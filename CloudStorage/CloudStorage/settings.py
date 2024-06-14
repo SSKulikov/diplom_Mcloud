1b6feecd84a6e7e161ba3a13c19f8929f3047b15
Sergey Kulikov <kylik11@mail.ru>
2024-06-14 06:00:13 +0300

    'ones'

--- CloudStorage/CloudStorage/settings.py
+++ CloudStorage/CloudStorage/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'assets/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR/'MEDIA_ROOT')
